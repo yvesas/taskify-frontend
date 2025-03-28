d1dcf4efc8c40df57b94f538b04e5c986b61d0c0
Yves <yves.as@gmail.com>
2025-03-28 12:32:25 -0300

    :goal_net: external error capture.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+export const setupGlobalErrorHandling = () => {
+  // WARN: Captura erros não tratados no  - geral
+  window.addEventListener("error", (event) => {
+    console.error("Uncaught error:", event.error);
+    // // FIXME: enviar a um serviço de monitoramento
+  });
+
+  // WARN: Captura rejeições de promises não tratadas
+  window.addEventListener("unhandledrejection", (event) => {
+    console.error("Unhandled promise rejection:", event.reason);
+    // FIXME: enviar a um serviço de monitoramento
+    event.preventDefault();
+  });
+};
+
+setupGlobalErrorHandling();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
